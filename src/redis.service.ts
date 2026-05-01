d6bf791d6154f66fb1dc3ac5d350e87e5e6829a7
mk-sdev <kaleta222@gmail.com>
2026-05-01 13:41:09 +0200

    fix(redis): add db config and close client connection

--- src/redis.service.ts
+++ src/redis.service.ts
@@ -1,15 +1,20 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import Redis from 'ioredis';
 
 @Injectable()
-export class RedisService implements OnModuleInit {
+export class RedisService implements OnModuleInit, OnModuleDestroy {
   private client: Redis;
 
   onModuleInit() {
     this.client = new Redis({
       host: process.env.REDIS_HOST || 'db',
       port: Number(process.env.REDIS_PORT) || 6379,
+      db: Number(process.env.REDIS_DB) || 0,
     });
+  }
+
+  async onModuleDestroy() {
+    await this.client.quit();
   }
 
   getClient() {
